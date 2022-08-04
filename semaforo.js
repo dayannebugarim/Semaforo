02b0f40a6722b0eb6766f5e72657684ad98ad28d
Dayanne <81201377+dayannebugarim@users.noreply.github.com>
2022-08-04 20:38:18 -0300

    mais ou menos

--- semaforo.js
+++ semaforo.js
@@ -1,35 +1,37 @@
-const buttons = document.getElementsByClassName("button");
 const red = document.getElementById("red");
 const yellow = document.getElementById("yellow");
 const green = document.getElementById("green");
 const auto = document.getElementById("automatic");
 const img = document.getElementById("img");
 
-console.log(buttons);
-console.log(red);
-console.log(img.src);
+var GreenInterval;
+var YellowInterval;
+var RedInterval;
+
+auto.addEventListener("click", () => {
+    GreenInterval = setInterval(() => {img.src="./img/verde.png"},1000);
+    YellowInterval = setInterval(() => {img.src="./img/amarelo.png"},2000);
+    RedInterval = setInterval(() => {img.src="./img/vermelho.png"},3000);    
+    GreenInterval = setInterval(() => {img.src="./img/verde.png"},4000);
+});
 
 red.addEventListener("click", () => {
     img.src="./img/vermelho.png";
+    clearInterval(GreenInterval);
+    clearInterval(YellowInterval);
+    clearInterval(RedInterval);
 });
 
 yellow.addEventListener("click", () => {
     img.src="./img/amarelo.png";
+    clearInterval(GreenInterval);
+    clearInterval(YellowInterval);
+    clearInterval(RedInterval);
 });
 
 green.addEventListener("click", () => {
     img.src="./img/verde.png";
-});
-
-auto.addEventListener("click", () => {
-    setInterval(() => {
-        img.src="./img/verde.png";
-    },1000)
-    setInterval(() => {
-        img.src="./img/amarelo.png";
-    },2000)
-    setInterval(() => {
-        img.src="./img/vermelho.png";
-    },3000)
-    //img.src="./img/verde.png";
+    clearInterval(GreenInterval);
+    clearInterval(YellowInterval);
+    clearInterval(RedInterval);
 });
